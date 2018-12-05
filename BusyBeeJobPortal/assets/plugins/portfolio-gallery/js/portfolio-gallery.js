256590956a1bbf0b15b6df0509865ef7b5bc8ba3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-12-05 17:51:48 +0500

    Update Added  Nesecary Work Environment files

---
+++ BusyBeeJobPortal/assets/plugins/portfolio-gallery/js/portfolio-gallery.js
@@ -0,0 +1,147 @@
+$(function(){
+        $("#elastic_grid_demo").elastic_grid({
+            'showAllText' : 'All',
+            'filterEffect': 'popup', // moveup, scaleup, fallperspective, fly, flip, helix , popup
+            'hoverDirection': true,
+            'hoverDelay': 0,
+            'hoverInverse': false,
+            'expandingSpeed': 500,
+            'expandingHeight': 500,
+            'items' :
+            [
+                
+                {
+                    'title'         : 'Koino',
+                    'description'   : 'Koino is a Cryptocurrency Website that helps you make Real Time Exchange between Ethereum and Bitcoin using Real Time Price Conversion. It is fast, efficient, and reliable for Bitcoin and Ethereum transactions. It is built on Laravel with Bootstrap 4 with all URLs encryption enabled to make transactions and users data secured.',
+                    'thumbnail'     : ['images/small/crypto/Koino/c2.jpg', 'images/small/crypto/Koino/c2-1.jpg', 'images/small/crypto/Koino/c2-2.jpg', 'images/small/crypto/Koino/c2-3.jpg', 'images/small/crypto/Koino/c2-4.jpg'],
+                    'large'         : ['images/large/crypto/Koino/c2.jpg', 'images/large/crypto/Koino/c2-1.jpg', 'images/large/crypto/Koino/c2-2.jpg', 'images/large/crypto/Koino/c2-3.jpg','images/large/crypto/Koino/c2-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['Cryptocurrency']
+                },
+                {
+                    'title'         : 'Restaurants',
+                    'description'   : 'A wide range of Restaurant based website themes that let you customize your favorite Restaurant according to your needs while keeping the user interface web and mobile friendly.',
+                    'thumbnail'     : ['images/small/website/Restaurant/w2.jpg', 'images/small/website/Restaurant/w2-1.jpg', 'images/small/website/Restaurant/w2-2.jpg', 'images/small/website/Restaurant/w2-3.jpg', 'images/small/website/Restaurant/w2-4.jpg'],
+                    'large'         : ['images/large/website/Restaurant/w2.jpg', 'images/large/website/Restaurant/w2-1.jpg', 'images/large/website/Restaurant/w2-2.jpg', 'images/large/website/Restaurant/w2-3.jpg', 'images/large/website/Restaurant/w2-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [
+                        ],
+                    'tags'          : ['Website']
+                },
+               
+                {
+                    'title'         : 'Hosting Plan',
+                    'description'   : 'At Hosting Plan you can actually compare the prices from various hosting providers and you can get the highest and lowest prices from them. It evaluates the best prices for you among all of the plateforms for domain registration and for hosting services along with their specifications. Elastic quantifiability & Pay as you employ evaluation. Reliable Support. Pay As You Go. Cash Back feature enabled. Durable, Safe & Secure.',
+                    'thumbnail'     : ['images/small/crypto/Hosting_Plan/c4.jpg', 'images/small/crypto/Hosting_Plan/c4-1.jpg', 'images/small/crypto/Hosting_Plan/c4-2.jpg', 'images/small/crypto/Hosting_Plan/c4-3.jpg','images/small/crypto/Hosting_Plan/c4-4.jpg'],
+                    'large'         : ['images/large/crypto/Hosting_Plan/c4.jpg', 'images/large/crypto/Hosting_Plan/c4-1.jpg', 'images/large/crypto/Hosting_Plan/c4-2.jpg', 'images/large/crypto/Hosting_Plan/c4-3.jpg','images/large/crypto/Hosting_Plan/c4-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [
+                        ],
+                    'tags'          : ['Cryptocurrency']
+                },
+                {
+                    'title'         : 'Flippy Boat UI',
+                    'description'   : 'Flippy Boat is the best diversion app speed up to create flips, collect coins! Catch a wave! Upgrade to travel quick, fly high and flip your boat to induce a lot of coins! Unlock new boats by finishing challenges.',
+                    'thumbnail'     : ['images/small/ui_ux/Flippy_Boat_Ui/u3.jpg', 'images/small/ui_ux/Flippy_Boat_Ui/u3-1.jpg', 'images/small/ui_ux/Flippy_Boat_Ui/u3-2.jpg', 'images/small/ui_ux/Flippy_Boat_Ui/u3-3.jpg', 'images/small/ui_ux/Flippy_Boat_Ui/u3-4.jpg'],
+                    'large'         : ['images/large/ui_ux/Flippy_Boat_Ui/u3.jpg', 'images/large/ui_ux/Flippy_Boat_Ui/u3-1.jpg', 'images/large/ui_ux/Flippy_Boat_Ui/u3-2.jpg', 'images/large/ui_ux/Flippy_Boat_Ui/u3-3.jpg', 'images/large/ui_ux/Flippy_Boat_Ui/u3-4.jpg'],
+                    'img_title'     : ['jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['UI/UX Design']
+                },
+                
+                {
+                    'title'         : 'Photography',
+                    'description'   : 'Online customizable Photography Templates help lots of Freelancers, Studios and Photographers worldwide who want to run their own websites to showcase their work to their target market.',
+                    'thumbnail'     : ['images/small/website/Photography/w1.jpg', 'images/small/website/Photography/w1-1.jpg', 'images/small/website/Photography/w1-2.jpg', 'images/small/website/Photography/w1-3.jpg', 'images/small/website/Photography/w1-4.jpg'],
+                    'large'         : ['images/large/website/Photography/w1.jpg', 'images/large/website/Photography/w1-1.jpg', 'images/large/website/Photography/w1-2.jpg', 'images/large/website/Photography/w1-3.jpg', 'images/large/website/Photography/w1-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [
+                        ],
+                    'tags'          : ['Website']
+                },
+                
+                {
+                    'title'         : 'MoonFolio',
+                    'description'   : 'Moonfolio helps you create the foremost of your crypto holdings. Use our token huntsman to make a varied cryptocurrency portfolio from scratch, take charge of your assets and maintain with their performance.',
+                    'thumbnail'     : ['images/small/crypto/Moon_Folio/c3.jpg', 'images/small/crypto/Moon_Folio/c3-1.jpg', 'images/small/crypto/Moon_Folio/c3-2.jpg', 'images/small/crypto/Moon_Folio/c3-3.jpg', 'images/small/crypto/Moon_Folio/c3-4.jpg'],
+                    'large'         : ['images/large/crypto/Moon_Folio/c3.jpg', 'images/large/crypto/Moon_Folio/c3-1.jpg', 'images/large/crypto/Moon_Folio/c3-2.jpg', 'images/large/crypto/Moon_Folio/c3-3.jpg', 'images/large/crypto/Moon_Folio/c3-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 1 ', 'jquery elastic grid 2', 'jquery elastic grid 3', 'jquery elastic grid 4', 'jquery elastic grid 5'],
+                    'button_list'   :
+                    [
+                        ],
+                    'tags'          : ['Cryptocurrency']
+                },
+                {
+                    'title'         : 'Paint Hit',
+                    'description'   : 'Paint Hit tests players temporal order skills through the deceivingly simple task of throwing paint and unharness the sport globally once a really short testing amount. Splash your drops of color on the tower and paint it all the way! fancy the gorgeous art vogue and casual gameplay. Screenshots area unit prepared here.',
+                    'thumbnail'     : ['images/small/ui_ux/Paint_Hit/u4.jpg', 'images/small/ui_ux/Paint_Hit/u4-1.jpg', 'images/small/ui_ux/Paint_Hit/u4-2.jpg', 'images/small/ui_ux/Paint_Hit/u4-3.jpg', 'images/small/ui_ux/Paint_Hit/u4-4.jpg'],
+                    'large'         : ['images/large/ui_ux/Paint_Hit/u4.jpg', 'images/large/ui_ux/Paint_Hit/u4-1.jpg', 'images/large/ui_ux/Paint_Hit/u4-2.jpg', 'images/large/ui_ux/Paint_Hit/u4-3.jpg', 'images/large/ui_ux/Paint_Hit/u4-4.jpg'],
+                    'img_title'     : ['jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['UI/UX Design']
+                },
+                {
+                    'title'         : 'Crypto Currency Trakker',
+                    'description'   : 'As the title explains, this website was built for tracking all of the cryptocurrencies that are in the market to keep track of their fluctuation. It is real time based that helps users mannage their all coins and alert them through alarms if their added coins prices get lower or higher than their purchased prices. Bundle of APIs are used to get the Real Time Graphs and Values.',
+                    'thumbnail'     : ['images/small/crypto/Crypto_trakker/c1.jpg', 'images/small/crypto/Crypto_trakker/c1-1.jpg', 'images/small/crypto/Crypto_trakker/c1-2.jpg', 'images/small/crypto/Crypto_trakker/c1-3.jpg', 'images/small/crypto/Crypto_trakker/c1-4.jpg'],
+                    'large'         : ['images/large/crypto/Crypto_trakker/c1.jpg', 'images/large/crypto/Crypto_trakker/c1-1.jpg', 'images/large/crypto/Crypto_trakker/c1-2.jpg', 'images/large/crypto/Crypto_trakker/c1-3.jpg', 'images/large/crypto/Crypto_trakker/c1-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 1 ', 'jquery elastic grid 2', 'jquery elastic grid 3', 'jquery elastic grid 4', 'jquery elastic grid 5'],
+                    'button_list'   :
+                    [
+                        ],
+                    'tags'          : ['Cryptocurrency']
+                },
+                {
+                    'title'         : 'Traveling',
+                    'description'   : 'Online highly customizable Traveling Templates help lots of Tourism and Traveling companies all over the world, to start the professional flow of their business. All of these templates can manage a broad plan of your tour, also display the tour prices and places beauty. Traveling based templates are User-Friendly, SEO Optimized, Reliable and completly Resposive. Some of samples are ready to see.',
+                    'thumbnail'     : ['images/small/website/Travel/w3.jpg', 'images/small/website/Travel/w3-1.jpg', 'images/small/website/Travel/w3-2.jpg', 'images/small/website/Travel/w3-3.jpg', 'images/small/website/Travel/w3-4.jpg'],
+                    'large'         : ['images/large/website/Travel/w3.jpg', 'images/large/website/Travel/w3-1.jpg', 'images/large/website/Travel/w3-2.jpg', 'images/large/website/Travel/w3-3.jpg', 'images/large/website/Travel/w3-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['Website']
+                },
+                
+                {
+                    'title'         : 'Racing Car Game',
+                    'description'   : 'The goal of this racer game is to drive your automotive at intervals four modes in a very 3D realistic setting that you simply can love. But, confine mind that you simply can need to drive your automotive terribly rigorously while not touch any vehicle otherwise you can lose sound simple. Here is the UI of this game.',
+                    'thumbnail'     : ['images/small/ui_ux/Racing_Car_Game/u1.jpg', 'images/small/ui_ux/Racing_Car_Game/u1-1.jpg', 'images/small/ui_ux/Racing_Car_Game/u1-2.jpg', 'images/small/ui_ux/Racing_Car_Game/u1-3.jpg','images/small/ui_ux/Racing_Car_Game/u1-4.jpg'],
+                    'large'         : ['images/large/ui_ux/Racing_Car_Game/u1.jpg', 'images/large/ui_ux/Racing_Car_Game/u1-1.jpg', 'images/large/ui_ux/Racing_Car_Game/u1-2.jpg','images/large/ui_ux/Racing_Car_Game/u1-3.jpg','images/large/ui_ux/Racing_Car_Game/u1-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['UI/UX Design']
+                },
+                {
+                    'title'         : 'Under Construction',
+                    'description'   : 'Here square measure a few samples of Construction Websites that our expert designers & developers have built. Take a look square measure given for showcase solely which are effective and eye catching for your audience.',
+                    'thumbnail'     : ['images/small/website/Under_Construction/w4.jpg', 'images/small/website/Under_Construction/w4-1.jpg', 'images/small/website/Under_Construction/w4-2.jpg', 'images/small/website/Under_Construction/w4-3.jpg', 'images/small/website/Under_Construction/w4-4.jpg'],
+                    'large'         : ['images/large/website/Under_Construction/w4.jpg', 'images/large/website/Under_Construction/w4-1.jpg', 'images/large/website/Under_Construction/w4-2.jpg', 'images/large/website/Under_Construction/w4-3.jpg', 'images/large/website/Under_Construction/w4-4.jpg'],
+                    'img_title'     : ['jquery elastic grid 6 ', 'jquery elastic grid 7 ', 'jquery elastic grid 8', 'jquery elastic grid 9', 'jquery elastic grid 9'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['Website']
+                },
+                {
+                    'title'         : '4X4 Jeep Game',
+                    'description'   : '4x4 machine drive rally on the uphill climb mountains. Drivers are gathered from all around the country with their offroad jeeps and monster trucks all lined up for the mountain drifting and rally sport.',
+                    'thumbnail'     : ['images/small/ui_ux/4X4_Jeep_Game/u2.jpg', 'images/small/ui_ux/4X4_Jeep_Game/u2-1.jpg', 'images/small/ui_ux/4X4_Jeep_Game/u2-2.jpg', 'images/small/ui_ux/4X4_Jeep_Game/u2-3.jpg', 'images/small/ui_ux/4X4_Jeep_Game/u2-4.jpg'],
+                    'large'         : ['images/large/ui_ux/4X4_Jeep_Game/u2.jpg', 'images/large/ui_ux/4X4_Jeep_Game/u2-1.jpg', 'images/large/ui_ux/4X4_Jeep_Game/u2-2.jpg', 'images/large/ui_ux/4X4_Jeep_Game/u2-3.jpg', 'images/large/ui_ux/4X4_Jeep_Game/u2-4.jpg'],
+                    'img_title'     : ['jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid', 'jquery elastic grid'],
+                    'button_list'   :
+                    [],
+                    'tags'          : ['UI/UX Design']
+                },
+                
+                
+                
+            ]
+        });
+    });
